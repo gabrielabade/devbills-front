daf04960f8cdd9870df0029a9c3cb944d7de7a86
gabrielabade <gabriela.bade@hotmail.com>
2024-04-08 17:17:29 -0300

    configurando o Styled Components e Tema com estilos globais

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,5 +1,7 @@
 import { createGlobalStyle } from 'styled-components';
 
+import { theme } from './theme';
+
 export default createGlobalStyle`
 *{
     margin: 0;
@@ -9,5 +11,23 @@ export default createGlobalStyle`
 }
 body{
     -webkit-font-smoothing: antialiased;
+    background-color: ${theme.colors.black};
+}
+
+body, input, button, select {
+    font: 1rem 'Lexend', 'sans-serif';
+}
+
+h1, h2, p, span, strong, button, label, input{
+    line-height: 100%;
+}
+
+#root {
+    max-width: 1280px;
+    margin: 0 auto;
+}
+
+button {
+    cursor: pointer;
 }
 `;
