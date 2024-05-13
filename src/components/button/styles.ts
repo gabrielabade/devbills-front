331cb6a495c4ed0a48d6ee9895177205dde3fc71
gabrielabade <gabriela.bade@hotmail.com>
2024-05-13 15:09:03 -0300

    FinancialEvolutionBarChart

--- src/components/button/styles.ts
+++ src/components/button/styles.ts
@@ -13,15 +13,15 @@ export const Container = styled.button<ContainerProps>`
   height: 2.25rem;
   border-radius: 0.25rem;
   background-color: ${(props) =>
-    props.$variant == 'default' ? theme.colors.primary : 'transparent'};
+    props.$variant === 'default' ? theme.colors.primary : 'transparent'};
   color: ${(props) =>
-    props.$variant == 'default' ? theme.colors.black : theme.colors.primary};
+    props.$variant === 'default' ? theme.colors.black : theme.colors.primary};
   border: 0;
   padding: 0 0.75rem;
   transition: all 100ms;
 
   ${(props) =>
-    props.$variant == 'outline' &&
+    props.$variant === 'outline' &&
     css`
       border: 1px solid ${theme.colors.primary};
     `}
